fb5113a2c040d276a72a4aa8c7e91bc400140db8
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2024-01-09 19:57:56 +0100

    "placerPionPlateau : tests ok"

--- Model/Plateau.py
+++ Model/Plateau.py
@@ -36,8 +36,8 @@ def type_plateau(plateau: list) -> bool:
 
 def construirePlateau()->list:
     """
-    fonction qui créé un tableau 2D vide avec nb_ligne et nb_colonne qui qui sont constantes.
-    :return:retourne une liste de liste avec nb_ligne et nb_colonne qui qui sont constantes.
+    fonction qui créé un tableau 2D vide avec nb_ligne et nb_colonne qui sont constantes.
+    :return:retourne une liste de liste avec nb_ligne et nb_colonne qui sont constantes.
     """
     plateau = []
     for ligne in range(const.NB_LINES):
@@ -47,5 +47,25 @@ def construirePlateau()->list:
         plateau.append(tab)
     return plateau
 
-
-
+def placerPionPlateau(plateau: list, pion: dict, num_col: int) -> int:
+    """
+    fonction qui dépose le pion dans la colonne indiquée
+    :param plateau: liste de liste qui définit le plateau du jeu
+    :param pion: dictionnaire qui définit la couleur et l'identifiant du pion
+    :param num_col: le numéro de colonne pour placer le pion dans le plateau
+    :return: retourne le numéro de ligne où se trouve le pion
+    """
+    if type(plateau) is not list:
+        raise TypeError("placerPionPlateau : Le premier paramètre ne correspond pas à un plateau")
+    if type(pion) is not dict:
+        raise TypeError("placerPionPlateau : Le second paramètre n’est pas un pion ")
+    if type(num_col) is not int:
+        raise TypeError("placerPionPlateau : Le troisième paramètre n’est pas un entier")
+    if num_col > const.NB_COLUMNS-1 and num_col < 0:
+        raise ValueError(f"placerPionPlateau : La valeur de la colonne {num_col} n’est pas correcte")
+    num_ligne = const.NB_LINES - 1
+    while num_ligne >= 0 and plateau[num_ligne][num_col] is not None:
+        num_ligne -= 1
+    if num_ligne >= 0:
+        plateau[num_ligne][num_col] = pion
+    return num_ligne
